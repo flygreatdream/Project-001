0bafb64b081c00fea2590a08823e6e03c5bd0e0f
Tong Guo <tongg@zedat.fu-berlin.de>
2016-10-29 15:19:08 +0200

    Update readme and create new folder

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Project-001
 New test project
+
+R verseion control, I am coming...
+
---
+++ Rscript/savanna.R
@@ -0,0 +1,6 @@
+#an R script
+
+
+perecover <- seq(0,1,0.2)
+shrubcover <- seq(1,0,-0.2)
+totalcover <- perecover+ shrubcover
